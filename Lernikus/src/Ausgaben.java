b701ccb45384b6a03e93acd94a0bdd6def6a6c6a
Feichtinger-Lucas <lu.feicht@gmail.com>
2018-04-17 14:33:32 +0200

    .

---
+++ Lernikus/src/Ausgaben.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.util.ArrayList;
+
+public class Ausgaben {
+	
+    public static void Anfang(){
+    	
+    	System.out.println("\t________Lernikus________");
+    	
+
+    }
+}
