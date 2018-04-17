63ffd977003cc4b94adb64fce794e63df1fc5ab2
KampmuellerPaul <paul.kampmueller@gmail.com>
2018-04-17 15:37:18 +0200

    17042018 - Kampmüller_Start

---
+++ Lernikus/src/Login.java
@@ -0,0 +1,27 @@
+public class Login {
+	private String benutzername;
+	private String password;
+	
+	public Login(String benutzername, String password) {
+		this.benutzername = benutzername;
+		this.password = password;
+	}
+
+	public String getBenutzername() {
+		return benutzername;
+	}
+
+	public void setBenutzername(String benutzername) {
+		this.benutzername = benutzername;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void einloggen
+}
